4242d3aaa9c2c7598ddcb8b778962e26ea0ba21b
wangsong <wanggyanmo@163.com>
2019-04-16 23:44:29 +0800

    增加交换变量的方法

--- Python/SwapTwoValue.py
+++ Python/SwapTwoValue.py
@@ -1,10 +1,34 @@
-def swap(value1, value2):
+# 方式一 中间变量
+def swap1(value1, value2):
+    temp = value1
+    value1 = value2
+    value2 = temp
+    return value1, value2
+
+
+tup = swap1(666, 999)
+print(tup)
+
+
+# 方式二 python特有
+def swap2(value1, value2):
     value1, value2 = value2, value1
     return value1, value2  # 返回元组，不需要加括号
 
 
 valueA = 10
 valueB = 20
-tup = swap(valueA, valueB)  # 元组
+tup = swap2(valueA, valueB)  # 元组
+print(tup)
 
+
+# 方式三 数学算法
+def swap3(value1, value2):
+    value1 = value1 - value2
+    value2 = value1 + value2  # value2 = (value1 - value2) + value2
+    value1 = value2 - value1  # value1 = value1 - (value1 - value2)
+    return value1, value2
+
+
+tup = swap3(1, 2)
 print(tup)
