8d1ddad026a1ade8de9a5569f495c512ec37d2de
wangsong <wanggyanmo@163.com>
2019-04-19 00:37:06 +0800

    OC实现Java的LinkList

---
+++ Objective-C++/LinkList/LinkList.h
@@ -0,0 +1,17 @@
+//
+//  LinkList.h
+//  LinkList
+//
+//  Created by sw on 2019/4/19.
+//  Copyright © 2019 WS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LinkList : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Objective-C++/LinkList/LinkList.m
@@ -0,0 +1,13 @@
+//
+//  LinkList.m
+//  LinkList
+//
+//  Created by sw on 2019/4/19.
+//  Copyright © 2019 WS. All rights reserved.
+//
+
+#import "LinkList.h"
+
+@implementation LinkList
+
+@end
