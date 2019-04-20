5dc9d921484e5e79a2a0fe4a019e2ff4ecf181ac
wangsong <wanggyanmo@163.com>
2019-04-21 00:37:19 +0800

    python实现Java的LinkList

--- Python/List/LinkList.py
+++ Python/List/LinkList.py
@@ -7,10 +7,46 @@ class Node:
 
 
 class LinkList:
-    __slots__ = ['first']
+    __slots__ = ['first', 'last', 'size']
+
+    @classmethod
+    def link_list(cls):
+        return cls()
+
     def __init__(self):
+        self.first = None
+        self.last = None
+        self.size = 0
+
+    def add(self, obj):
         pass
 
+    def remove_at_index(self, idx):
+        pass
+
+    def remove(self, obj):
+        pass
+
+    def pop(self):
+        pass
+
+    def object_at_index(self, idx):
+        pass
+
+    def index_of(self, obj):
+        pass
+
+    def contains(self, obj):
+        pass
+
+    def is_empty(self):
+        return self.size == 0
+
+    def clear(self):
+        self.first = None
+
+    def size(self):
+        return self.size
 
 
 list = LinkList()
