955a1ed22e997d8791411e7a0c9cbcd83d53118a
wangsong <wanggyanmo@163.com>
2019-04-22 22:46:42 +0800

    python实现Java的LinkList

--- Python/List/LinkList.py
+++ Python/List/LinkList.py
@@ -11,24 +11,20 @@ class Node:
 
 
 class LinkList:
-    __slots__ = ['first', 'last', 'size']
-
-    @classmethod
-    def link_list(cls):
-        return cls()
+    __slots__ = ['first', 'last', '__size']
 
     def __init__(self):
         self.first = None
         self.last = None
-        self.size = 0
+        self.__size = 0
 
     def add(self, obj):
-        if self.size == 0:
+        if self.__size == 0:
             self.first = Node(obj, self.first)
         else:
-            prev = self.__node(self.size - 1)
-            prev.next = Node(obj, prev.next)
-        self.size += 1
+            prev = self.__node(self.__size - 1)
+            prev.next = Node(obj, None)
+        self.__size += 1
 
     def insert(self, obj, idx):
         if idx == 0:
@@ -36,7 +32,7 @@ class LinkList:
         else:
             prev = self.__node(idx - 1)
             prev.next = Node(obj, prev.next)
-        self.size += 1
+        self.__size += 1
 
     def remove_at_index(self, idx):
         if idx == 0:
@@ -44,17 +40,17 @@ class LinkList:
         else:
             prev = self.__node(idx - 1)
             prev.next = prev.next.next
-        self.size -= 1
+        self.__size -= 1
 
     def remove(self, obj):
         prev = self.__prev_node_of(obj)
         prev.next = prev.next.next
-        self.size -= 1
+        self.__size -= 1
 
     def pop(self):
-        prev = self.__node(self.size - 1)
+        prev = self.__node(self.__size - 1)
         prev.next = prev.next.next
-        self.size -= 1
+        self.__size -= 1
 
     def object_at_index(self, idx):
         node = self.__node(idx)
@@ -66,32 +62,32 @@ class LinkList:
 
     def contains(self, obj):
         node = self.first
-        for _ in range(0, self.size - 1):
+        for _ in range(0, self.__size - 1):
             if node.data is obj:
                 return True
             node = node.next
         return False
 
     def is_empty(self):
-        return self.size == 0
+        return self.__size == 0
 
     def clear(self):
         self.first = None
-        self.size = 0
+        self.__size = 0
 
     def size(self):
-        return self.size
+        return self.__size
 
     def __node(self, idx):
         node = self.first
-        for i in range(0, self.size - 1):
+        for i in range(0, idx):
             node = node.next
         return node
 
     def __node_of(self, obj):
         node = self.first
 
-        for _ in range(0, self.size - 1):
+        for _ in range(0, self.__size - 1):
             if node.data is obj:
                 return node
             node = node.next
@@ -100,15 +96,35 @@ class LinkList:
     def __prev_node_of(self, obj):
         node = self.first
         prev = None
-        for _ in range(0, self.size - 1):
+        for _ in range(0, self.__size - 1):
             if node.data is obj:
                 return node
             prev = node
             node = node.next
         return prev
 
+    @classmethod
+    def link_list(cls):
+        return cls()
 
-list = LinkList()
-list.__element = [2]
+# list = LinkList()
+# list.__element = [2]
+#
+# print(list.__element)
 
-print(list.__element)
+
+list = LinkList.link_list()
+list.add('1')
+list.add('2')
+list.add('3')
+print(list.size())
+list.insert('0', 1)
+print(list.size())
+list.insert('0', 0)
+print(list.size())
+list.insert('5', 5)
+print(list.size())
+
+
+
+
